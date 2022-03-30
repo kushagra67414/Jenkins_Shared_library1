5c1331b38fd6f165ae25a850194644003de80fea
Kushagra Bansal <46487696+kushagra67414@users.noreply.github.com>
2022-03-30 16:28:25 +0530

    Create jenkinsforMaven.groovy

---
+++ vars/jenkinsforMaven.groovy
@@ -0,0 +1,37 @@
+def call(String repoUrl) {
+  pipeline {
+       agent any
+       tools {
+           maven 'Maven 3.5.0'
+           }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh "mvn --version"
+                   sh "java -version"
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'master',
+                       url: "${repoUrl}"
+               }
+           }
+           stage("Cleaning workspace") {
+               steps {
+                   sh "mvn clean"
+               }
+           }
+           stage("Running Testcase") {
+              steps {
+                   sh "mvn test"
+               }
+           }
+           stage("Packing Application") {
+               steps {
+                   sh "mvn package -DskipTests"
+               }
+           }
+       }
+   }
+}
